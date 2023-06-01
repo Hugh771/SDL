14af67a113aaa40e0910e0fc0879bc93ce0baf7d
Jason <937617839@qq.com>
2023-06-01 22:44:11 +0800

    code

---
+++ sdlapp/main/routes.py
@@ -0,0 +1,10 @@
+from flask import redirect,render_template,url_for,jsonify
+from . import main_bp
+
+@main_bp.route('/login')
+def login():
+    return jsonify()
+
+@main_bp.route('/logout')
+def louout():
+    return jsonify()
