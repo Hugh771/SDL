a5748a558004eebf7e2a19a0346aca22c721244b
tianyang.liu <tianyang.liu@ln.ad>
2023-06-19 18:25:37 +0800

    jwt

--- sdlapp/main/routes.py
+++ sdlapp/main/routes.py
@@ -1,16 +1,20 @@
 from flask import redirect,render_template,url_for,jsonify,request
 from . import main_bp
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required,create_access_token
+from sdlapp.auth.auth import jwt,user_required
 
 @main_bp.route('/login')
 def login():
     if request.method == 'GET':
         render_template()
     if request.method == 'POST':
-        return jsonify({'login':1})
+        json = request.get_json()
+        if json['username'] == 'Jason' and json['passwd'] == '123':
+            return jsonify({'login':1})
 
 @main_bp.route('/view')
 @jwt_required()
+#@user_required()
 def view():
     return jsonify({'view':1})
 
