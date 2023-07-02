c7d4c2bce2fc4e7af2d2e83ca6ccc2505ff2617a
Jason <937617839@qq.com>
2023-07-02 19:32:42 +0800

    auth

--- sdlapp/auth/auth.py
+++ sdlapp/auth/auth.py
@@ -6,11 +6,17 @@ jwt = JWTManager()
 #权限信息封装
 @jwt.additional_claims_loader
 def add_claims_to_access_token(identity):
-    pass
+    print('user is {}'.format(identity))
+    return {'roles':'admin'}
 
 #权限检验
-def user_required(fn):
+def permissions_required(fn):
     @wraps(fn)
     def wrapper(*args,**kwargs):
-         identity = get_jwt_identity()
+        identity = get_jwt_identity()
+        print(identity)
+        r = get_jwt()
+        print(r)
+        return fn(*args,**kwargs)
+    return wrapper
 
--- sdlapp/main/routes.py
+++ sdlapp/main/routes.py
@@ -1,24 +1,26 @@
 from flask import redirect,render_template,url_for,jsonify,request
 from . import main_bp
 from flask_jwt_extended import jwt_required,create_access_token
-from sdlapp.auth.auth import jwt,user_required
+from sdlapp.auth.auth import jwt,permissions_required
 
 @main_bp.route('/login')
 def login():
-    if request.method == 'GET':
-        render_template()
-    if request.method == 'POST':
-        json = request.get_json()
-        if json['username'] == 'Jason' and json['passwd'] == '123':
-            return jsonify({'login':1})
-
+    # if request.method == 'GET':
+    #     render_template()
+    # if request.method == 'POST':
+    #     json = request.get_json()
+    #     if json['username'] == 'Jason' and json['passwd'] == '123':
+    #         return jsonify({'login':1})
+    access_token = create_access_token(identity='admin')
+    return jsonify(access_token=access_token),200
 @main_bp.route('/view')
 @jwt_required()
-#@user_required()
+@permissions_required
 def view():
     return jsonify({'view':1})
 
 @main_bp.route('/logout')
 @jwt_required()
+@permissions_required
 def louout():
     return jsonify({'logout':1})
