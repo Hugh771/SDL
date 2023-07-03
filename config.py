638aee6b45d52d584bc242f0c9d1cb4e6bdb290e
Jason <937617839@qq.com>
2023-07-03 22:41:21 +0800

    models

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
 Config = {
-    'SQLALCHEMY_DATABASE_URI':'mysql+pymysql://root:123456@!#@192.168.52.130:3306/test'
+    'SQLALCHEMY_DATABASE_URI':'mysql+pymysql://root:123456@!#@192.168.52.130:3306/test',
+    'JWT_SECRET_KEY':'kOSNs01zR5BGmM9rgFTP5UzqRodnuCFo',
+    'JWT_ACCESS_TOKEN_EXPIRES':timedelta(minutes=60)
 }
--- sdlapp/main/models.py
+++ sdlapp/main/models.py
@@ -1,10 +1,19 @@
 from sdlapp import db
+from datetime import datetime
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'user'
+
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    passwd = db.Column(db.String(120), unique=True, nullable=False)
-
+    passwd_hash = db.Column(db.String(128), unique=True, nullable=False)
+    role = db.Column(db.String(80), default='active')
+    last_login = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    phone_number = db.Column(db.String(20), nullable=True)
+    api_key = db.Column(db.String(128), nullable=True)
+    custom_attributes = db.Column(db.JSON, nullable=True)
     def __repr__(self):
         return '<User %r>' %self.username
